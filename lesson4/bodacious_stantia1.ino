f7cfb70b725d3a7f982b7c8421b8d4af2deaea06
unknown <you@example.com>
2020-06-24 10:27:28 +0800

    第六次作业by rzy

--- lesson4/bodacious_stantia1.ino
+++ lesson4/bodacious_stantia1.ino
@@ -2,10 +2,12 @@
 #define IN2 3
 #define IN3 4
 #define IN4 5
+
 #define S1 8
 #define S2 9
 #define S3 10
 #define S4 11
+
 #define LT 6
 #define BT 7
 void setup()
@@ -14,43 +16,43 @@ void setup()
   pinMode(IN2, OUTPUT);//输入2
   pinMode(IN3, OUTPUT);//输入3
   pinMode(IN4, OUTPUT);//输入4
+
   pinMode(S1, OUTPUT);//片选1
   pinMode(S2, OUTPUT);//片选2
   pinMode(S3, OUTPUT);//片选3
   pinMode(S4, OUTPUT);//片选4
+  
   pinMode(LT,OUTPUT);//测试
   pinMode(BT,OUTPUT);//消隐
+  
   digitalWrite(LT,HIGH);
   digitalWrite(BT,HIGH);
+  
+
   digitalWrite(S1,HIGH);
   digitalWrite(S2,HIGH);
   digitalWrite(S3,HIGH);
   digitalWrite(S4,HIGH);
-  digitalWrite(S1,LOW);
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,LOW);
-  digitalWrite(S1,HIGH);
-    digitalWrite(S2,LOW);
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,LOW);
-  digitalWrite(S2,HIGH);
-    digitalWrite(S3,LOW);
-  digitalWrite(IN1,HIGH);
-  digitalWrite(IN2,HIGH);
-  digitalWrite(IN3,LOW);
-  digitalWrite(IN4,LOW);
-  digitalWrite(S3,HIGH);
-    digitalWrite(S4,LOW);
-  digitalWrite(IN1,LOW);
-  digitalWrite(IN2,LOW);
-  digitalWrite(IN3,HIGH);
-  digitalWrite(IN4,LOW);
-  digitalWrite(S4,HIGH);
+  
+  Serial.begin(9600);
 }
+
+byte income=0;
 void loop()
 {
+  String StringOne=Serial.readString();
+  for(int m=4;m>0;m--)
+  {
+    
+    income=StringOne.charAt(m-1);
+    digitalWrite(m+7,LOW);
+    income=income-'0';
+    digitalWrite(IN1,income&0x1);
+    digitalWrite(IN2,(income>>1)&0x1);
+    digitalWrite(IN3,(income>>2)&0x1);
+    digitalWrite(IN4,(income>>3)&0x1);
+    digitalWrite(m+7,HIGH);
+    
+  }
+
 }
---
+++ lesson6/lcd.c
@@ -0,0 +1,22 @@
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("START");
+  delay(500);
+  lcd.setCursor(0,0);
+  lcd.print("Please input. ");
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  if(Serial.available()>0)
+  {
+  	lcd.setCursor(0,1);
+    lcd.print(Serial.readString());
+  }
+}
